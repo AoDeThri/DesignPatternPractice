8a619923ac7add0ba9614381234e66af4cbce2ea
aodethri <740950342@qq.com>
2020-10-27 21:18:12 +0800

    feat: observer pattern
    add Weather class as an example of observer pattern

---
+++ src/design/observer/DisplayElement.java
@@ -0,0 +1,5 @@
+package design.observer;
+
+public interface DisplayElement {
+    void display();
+}
---
+++ src/design/observer/WeatherData.java
@@ -0,0 +1,59 @@
+package design.observer;
+
+import java.util.ArrayList;
+
+public class WeatherData implements Subject{
+
+    private float temperature;
+    private float humidity;
+    private float pressure;
+    private ArrayList observers;
+
+    public WeatherData(){
+        observers = new ArrayList();
+    }
+
+    public float getTemperature(){
+        return temperature;
+    }
+
+    public float getHumidity(){
+        return humidity;
+    }
+
+    public float getPressure(){
+        return pressure;
+    }
+
+    public void measurementsChanged(){
+        notifyObservers();
+    }
+
+    public void setMeasurements(float temperature, float humidity, float pressure){
+        this.temperature = temperature;
+        this.humidity = humidity;
+        this.pressure = pressure;
+        measurementsChanged();
+    }
+
+    @Override
+    public void registerObserver(Observer o) {
+        observers.add(o);
+    }
+
+    @Override
+    public void removeObserver(Observer o) {
+        int i = observers.indexOf(o);
+        if(i >= 0){
+            observers.remove(i);
+        }
+    }
+
+    @Override
+    public void notifyObservers() {
+        for (Object o : observers) {
+            Observer observer = (Observer) o;
+            observer.update(temperature, humidity, pressure);
+        }
+    }
+}
